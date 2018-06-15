f07357dc4952a487437d0f2804163086f550bf4c
vid9 <vid.cermelj@gmail.com>
2018-06-15 15:20:31 +0200

    mongo job fix

--- dist/bin/clearDayXp.js
+++ dist/bin/clearDayXp.js
@@ -1,6 +1,13 @@
 #!/app/bin/env node
 
 let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+require("./Uporabnik");
 let Uporabnik = mongoose.model("Uporabnik");
 
 console.log("Deleting daily xp");
