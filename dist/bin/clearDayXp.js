17e279d42d17d4a080d671bf5260b93d74ed1db8
vid9 <vid.cermelj@gmail.com>
2018-06-18 10:05:31 +0200

    .

--- dist/bin/clearDayXp.js
+++ dist/bin/clearDayXp.js
@@ -1,10 +1,26 @@
 #!/app/bin/env node
 
+require( '../models/db' );
+var mongoose = require( 'mongoose' );
+let Uporabnik = mongoose.model("Uporabnik");
+
+console.log("Deleting daily xp");
+
+Uporabnik.where().updateMany({ $set: { "dayXp": 0 } }, function (err, res) {
+    if (err) {
+        console.log(err);
+    } else { console.log(res); }
+    console.log("Daily xp deleted");
+    });
+
+
+
+/*
 let  mongo = require('mongodb');
 
 console.log("Deleting daily xp");
 
-mongo.connect(process.env.DB_url, {}, function(error, db){
+mongo.connect(process.env.DB_url, function(error, db){
     if (error) {
         console.log(error);
     }
@@ -15,4 +31,4 @@ mongo.connect(process.env.DB_url, {}, function(error, db){
         } else { console.log(res); }
         console.log("Daily xp deleted");
     });
-  });
+  });*/
