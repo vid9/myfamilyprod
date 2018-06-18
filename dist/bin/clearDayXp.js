b455ac9ca75ebeac069a3ce26fa94eb8ea24c381
vid9 <vid.cermelj@gmail.com>
2018-06-18 09:31:22 +0200

    testing vanilla mongodb

--- dist/bin/clearDayXp.js
+++ dist/bin/clearDayXp.js
@@ -1,23 +1,17 @@
 #!/app/bin/env node
 
-let mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
-
-require("../models/Uporabnik");
-let Uporabnik = mongoose.model("../models/Uporabnik");
+let  mongo = require('mongodb');
 
 console.log("Deleting daily xp");
-deleteXp();
 
-function deleteXp() {
-    Uporabnik.where().updateMany({ $set: { "dayXp": 0 } }, function (err, res) {
-    if (err) {
-        console.log(err);
-    } else { console.log(res); }
-    console.log("Daily xp deleted");
+mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db){
+    db.addListener("error", function(error){
+      console.log("Error connecting to MongoLab");
     });
-}
+    db.Uporabnik.updateMany({ $set: { "dayXp": 0 } }, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else { console.log(res); }
+        console.log("Daily xp deleted");
+    });
+  });
