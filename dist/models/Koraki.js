f1cea26561e8db701f80d3a0a2f750ab1477af1c
vid9 <vid.cermelj@gmail.com>
2018-07-03 16:09:13 +0200

    koraki

--- dist/models/Koraki.js
+++ dist/models/Koraki.js
@@ -3,7 +3,7 @@ let mongoose = require("mongoose");
 let korakiSchema = new mongoose.Schema({
     uporabnik: {type : mongoose.Schema.Types.ObjectId, ref : "Uporabnik"},
     druzina: {type:String, required: true},
-    koraki: [{type : mongoose.Schema.Types.ObjectId, ref : "Uporabnik"}]
+    koraki: [Number, Number, Number, Number, Number, Number, Number]
 });
 
-mongoose.model('Koraki', korakiaSchema, 'Koraki');
+mongoose.model('Koraki', korakiSchema, 'Koraki');
